250450a4889f627ba186a0967605597eb085f382
Kamila-Vieira <vkamila54@outlook.com>
2023-01-13 20:28:58 -0300

    Atualizando conteúdo da aula 4

--- modulo_04/aulas/aulas_desafio/pygame/futIGTI.py
+++ modulo_04/aulas/aulas_desafio/pygame/futIGTI.py
@@ -31,19 +31,24 @@ ball_y = ((height_field/2) - width_ball) + 10
 player1_y = (height_field/2) - width_player
 player2_y = (height_field/2) - width_player
 
-player_moveup = False
-player_movedown = False
+player1_moveup = False
+player1_movedown = False
+player2_moveup = False
+player2_movedown = False
 
-def move_player():
+ball_dir_x = -3
+ball_dir_y = 1
+max_down = height_field - height_player
+
+def move_player1():
     global player1_y
-    max_down = height_field - height_player
-    
-    if player_moveup:
+
+    if player1_moveup:
         player1_y -= 5
     else:
         player1_y += 0
         
-    if player_movedown:
+    if player1_movedown:
         player1_y += 5
     else:
         player1_y += 0
@@ -52,14 +57,41 @@ def move_player():
         player1_y = 0
     elif player1_y >= max_down:
         player1_y = max_down
+        
+def move_player2():
+    global player2_y
+    player2_y = ball_y
+    
+    if player2_y <= 0:
+        player2_y = 0
+    elif player2_y >= max_down:
+        player2_y = max_down
 
- 
 def move_ball():
     global ball_x
     global ball_y
+    global ball_dir_x
+    global ball_dir_y
     
-    ball_x += 1
-
+    ball_x += ball_dir_x
+    ball_y += ball_dir_y
+    
+    if ball_x < (width_player*2) - 23:
+        if player1_y < ball_y + 23:
+            if player1_y + 146 > ball_y:
+                ball_dir_x *= -1
+    
+    if ball_x > width_field - (2*width_player):
+        if player2_y < ball_y + 23:
+            if player2_y + 146 > ball_y:
+                ball_dir_x *= -1   
+    
+    if ball_y > 685:
+        ball_dir_y *= -1
+    elif ball_y <= 0:
+        ball_dir_y *= -1
+        
+    
 def drawn():
     window.blit(field, (0, 0))
     window.blit(player1, (width_player, player1_y))
@@ -75,16 +107,17 @@ while loop:
             loop = False
         if events.type == pygame.KEYDOWN:
             if events.key == pygame.K_w:
-                player_moveup = True
+                player1_moveup = True
             if events.key == pygame.K_s:
-                player_movedown = True
+                player1_movedown = True
         if events.type == pygame.KEYUP:
             if events.key == pygame.K_w:
-                player_moveup = False
+                player1_moveup = False
             if events.key == pygame.K_s:
-                player_movedown = False
+                player1_movedown = False
             
     drawn()
-   #move_ball()
-    move_player()
+    move_ball()
+    move_player1()
+    move_player2()
     pygame.display.update()
